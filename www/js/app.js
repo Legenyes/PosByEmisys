6640630ea425d85b9050eeb8b639b3f8f75a0907
Sébastien Lévêque <sleveque@emisys.eu>
2016-06-03 08:15:24 +0200

    First Commit

---
+++ www/js/app.js
@@ -0,0 +1,79 @@
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.basket', {
+    url: '/basket',
+    views: {
+      'tab-basket': {
+        templateUrl: 'templates/tab-basket.html',
+        controller: 'BasketCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.settings', {
+    url: '/settings',
+    views: {
+      'tab-settings': {
+        templateUrl: 'templates/tab-settings.html',
+        controller: 'SettingsCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/basket');
+
+});
+
---
+++ www/js/controllers.js
@@ -0,0 +1,81 @@
+angular.module('starter.controllers', ['ngCart'])
+
+        .controller('BasketCtrl', function ($scope, ngCart) {
+            $scope.products = [];
+            $scope.productPerRow = 4;
+
+            $scope.loadProducts = function () {
+                for (var i = 0; i < 16; i++) {
+                    $product = {
+                        'id': i,
+                        'name': 'Bière' + i,
+                        'orderAvailable': '1',
+                        'isOnSite': '1',
+                        'price': (0.33 * i) + 1,
+                        'picture': 'http://fakeimg.pl/200x200/?text=' + 'Bière' + i
+                    };
+                    $scope.products.push($product);
+                }
+            }
+
+            $scope.range = function (min, max, step) {
+                step = step || 1;
+                var input = [];
+                for (var i = min; i <= max; i += step) {
+                    input.push(i);
+                }
+                return input;
+            };
+
+            $scope.openPopover = function ($event) {
+                console.log('etest');
+            };
+            
+            $scope.refreshBasketScroll = function () {
+                var height = window.innerHeight;
+                var basket_height = height - 250;
+                var basketObj = document.getElementById('basketScroll');
+                basketObj.style.height = basket_height+"px";
+            };
+            
+            $scope.refreshCatalogScroll = function () {
+                var height = window.innerHeight;
+                var catalog_height = height - 100;
+                var catalogObj = document.getElementById('catalogScroll');
+                catalogObj.style.height = catalog_height+"px";
+            };
+            
+           
+            
+            $scope.emptyBasket = function () {
+                ngCart.empty();
+            }
+
+            ngCart.setShipping(0);
+            ngCart.setTaxRate(21);
+        })
+
+        .controller('ChatsCtrl', function ($scope, Chats) {
+            // With the new view caching in Ionic, Controllers are only called
+            // when they are recreated or on app start, instead of every page change.
+            // To listen for when this page is active (for example, to refresh data),
+            // listen for the $ionicView.enter event:
+            //
+            //$scope.$on('$ionicView.enter', function(e) {
+            //});
+
+            $scope.chats = Chats.all();
+            $scope.remove = function (chat) {
+                Chats.remove(chat);
+            };
+        })
+
+        .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+            $scope.chat = Chats.get($stateParams.chatId);
+        })
+
+        .controller('SettingsCtrl', function ($scope) {
+            $scope.settings = {
+                enableFriends: true
+            };
+        });
